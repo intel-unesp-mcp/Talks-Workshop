59d90ba2735dc54154da471dc0c9ca1eb6b6d0e8
Silvio Luiz Stanzani <silvio@phi02.ncc.unesp.br>
2016-07-18 18:57:22 -0400

    vectorization course

--- Vectorization/matrix-multi/vec.c
+++ Vectorization/matrix-multi/vec.c
@@ -13,7 +13,17 @@ void foo(int n){
   } 
 }
 
+struct coordinate {
+    float x, y, z;
+} aosobj[40000];
+
+struct coordinate2 {
+    float x[40000], y[40000], z[40000];
+} soaobj;
+
 int main(){
+
+
 
   foo(1000);
   int i,j,k,msize;
@@ -34,12 +44,38 @@ randV=rand();
 randV=randV*0.11;
 printf("randV %f\n", randV);
 
-init(a, b, c, msize, randV);
+
+ for(j=0; j<10000; j++) {
+
+ for(i=0; i<40000; i++) {
+   aosobj[i].x=i + randV;    
+   aosobj[i].y=i*i+ randV;  
+   aosobj[i].z=i+i+ randV;
+ }
+ for(i=0; i<40000; i++) {
+   soaobj.x[i]=i+i+ randV;
+   soaobj.y[i]=i-i+ randV;
+   soaobj.z[i]=i*i+ randV;
+}
+randV=rand();
+randV=randV*0.11;
+
+ for(i=0; i<40000; i++) {
+   aosobj[i].x=    aosobj[i].y+  aosobj[i].z + randV;
+ }
+
+ for(i=0; i<40000; i++) {
+
+   soaobj.x[i]= soaobj.y[i]+ soaobj.z[i] + randV;
+ }
+}
+
+//init(a, b, c, msize, randV);
 
 //sum(a, b, c, msize);
-sumLoopTiling(a, b, c, msize);
+//sumLoopTiling(a, b, c, msize);
 
-multv1(a, b, c, msize);
+//multv1(a, b, c, msize);
 
 _mm_free(a);
 _mm_free(b);
